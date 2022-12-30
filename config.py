087d9ca3fb5a444a6135095e62bc13433fec0e8b
FanerkaKOD <NikonorovArtem2006@yandex.ru>
2022-12-31 01:09:17 +0300

    DiscordBot
    Bot

---
+++ config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = "API KEY"  # Your Api Key
+ID_POST = 0  # Your Post ID
+USER_ROLES_LIST = ()
+MAX_ROLES = 10
+
+ROLES_LIST = {  # Your roles ID and emoji
+    "": 0,
+    "": 1,
+    "": 2,
+}
